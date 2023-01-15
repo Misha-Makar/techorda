e2d22f8dc17256cee4deb8a08bb0234cbfca0e32
Misha-Makar <meshituly@gmail.com>
2023-01-15 22:26:11 +0600

    lab5

--- classroom/test.py
+++ classroom/test.py
@@ -1,49 +1,15 @@
-from typing import List
+import pandas as pd
+
+df = pd.read_excel(r'mid.xlsx')
+df = pd.DataFrame(df, columns=['city'])
+print(df)
+lst = []
+for col in df['city']:
+    lst.append(col)
+
+from collections import Counter
+print(len(Counter(lst)))
 
 
-class Driver:
-
-    def __init__(self, rating: int):
-        self.rating = rating
-
-    def can_cook(self):
-        print('I can cook!!!')
-
-    def can_drive(self) -> None:
-        print('I can drive!!!')
 
 
-class Builder:
-
-    def __init__(self, equipments: List[str]):
-        self.equipments = equipments
-
-    def can_build(self) -> None:
-        print('I can build!!!')
-
-    def can_cook(self):
-        print('I can cook also!!!')
-
-
-class Person(Driver, Builder):
-
-    def __init__(self, rating: int, equipments: List[str], name: str, surname: str):
-        Driver.__init__(self, rating)
-        Builder.__init__(self, equipments)
-        self.name = name
-        self.surname = surname
-
-    def can_cook(self):
-        Driver.can_cook(self)
-        Builder.can_cook(self)
-        print('I can cook so amazing!')
-
-
-person = Person(rating=5, equipments=['Молоток', 'Каска'], name='Aibar', surname='Bek')
-# person.can_cook()
-# person.can_drive()
-# person.can_build()
-print(person.rating)
-print(person.equipments)
-print(person.name)
-print(person.surname)
